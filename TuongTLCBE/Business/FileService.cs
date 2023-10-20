750f3403f25cbc4b57742f81dd561dd9f8fbd797
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-10-20 09:22:31 +0700

    Update FileService.cs

--- TuongTLCBE/Business/FileService.cs
+++ TuongTLCBE/Business/FileService.cs
@@ -5,11 +5,13 @@ namespace TuongTLCBE.Business;
 
 public class FileService
 {
-    private readonly FileRepo _fileRepo;
-    private readonly DecodeToken _decodeToken;
     private static readonly string Endpoint =
         Environment.GetEnvironmentVariable("EndPoint", EnvironmentVariableTarget.Process)
         ?? throw new ArgumentException("Environment variable not found.");
+
+    private readonly DecodeToken _decodeToken;
+    private readonly FileRepo _fileRepo;
+
     public FileService(FileRepo fileRepo, DecodeToken decodeToken)
     {
         _fileRepo = fileRepo;
@@ -20,8 +22,8 @@ public class FileService
     {
         try
         {
-            string userId = _decodeToken.Decode(token, "userid");
-            List<FileUpload> fileList = await _fileRepo.GetFilesByUser(Guid.Parse(userId));
+            var userId = _decodeToken.Decode(token, "userid");
+            var fileList = await _fileRepo.GetFilesByUser(Guid.Parse(userId));
             return fileList;
         }
         catch (Exception e)
@@ -35,19 +37,17 @@ public class FileService
         try
         {
             List<string> urls = new();
-            string userId = _decodeToken.Decode(token, "userid");
+            var userId = _decodeToken.Decode(token, "userid");
             foreach (var file in files)
             {
-                string urlPath = Endpoint + ":8080/FileStorage/Pictures/";
-                Guid id = Guid.NewGuid();
-                string filename = id + Path.GetExtension(file.FileName);
-                urls.Add( urlPath + userId +"/"+ filename);
-                bool checkFolder = Directory.Exists(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\"+userId);
+                var urlPath = Endpoint + ":8080/FileStorage/Pictures/";
+                var id = Guid.NewGuid();
+                var filename = id + Path.GetExtension(file.FileName);
+                urls.Add(urlPath + userId + "/" + filename);
+                var checkFolder = Directory.Exists(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\" + userId);
                 if (!checkFolder)
-                {
-                    Directory.CreateDirectory(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\"+userId);
-                }
-                string desPath = Path.Combine(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures", userId, filename);
+                    Directory.CreateDirectory(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\" + userId);
+                var desPath = Path.Combine(@"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures", userId, filename);
                 /*
                 await file.CopyToAsync(new FileStream(desPath, FileMode.Create));
                 */
@@ -56,6 +56,7 @@ public class FileService
                 {
                     await sourceStream.CopyToAsync(destinationStream);
                 }
+
                 FileUpload fileIn = new()
                 {
                     Id = id,
@@ -65,6 +66,7 @@ public class FileService
                 };
                 _ = await _fileRepo.Insert(fileIn);
             }
+
             return urls;
         }
         catch (Exception e)
@@ -77,22 +79,20 @@ public class FileService
     {
         try
         {
-            string userid = _decodeToken.Decode(token, "userid");
-            FileUpload? fileUploadGet = await _fileRepo.GetFileByPath(url);
+            var userid = _decodeToken.Decode(token, "userid");
+            var fileUploadGet = await _fileRepo.GetFileByPath(url);
             if (fileUploadGet != null)
             {
-                if (!fileUploadGet.UploadedBy.Equals(Guid.Parse(userid)))
-                {
-                    return "This file isn't your to delete!";
-                }
-                bool deleteInDb = DeleteImgInStorage(url, token);
-                if (deleteInDb)
-                {
-                   return (await _fileRepo.Delete(fileUploadGet)) > 0;
-                }
-
-                return "Failed to delete file in storage!";
+                if (!fileUploadGet.UploadedBy.Equals(Guid.Parse(userid))) return "This file isn't your to delete!";
+                // bool deleteInDb = DeleteImgInStorage(url, token);
+                // if (deleteInDb)
+                // {
+                //    return (await _fileRepo.Delete(fileUploadGet)) > 0;
+                // }
+                return await _fileRepo.Delete(fileUploadGet) > 0;
+                // return "Failed to delete file in storage!";
             }
+
             return "File not found!";
         }
         catch (Exception e)
@@ -103,40 +103,32 @@ public class FileService
 
     private bool DeleteImgInStorage(string imgUrl, string token)
     {
-
         try
         {
-            string userid = _decodeToken.Decode(token, "userid");
-            string deletePath = "https://tuongtlc.ddns.net:8080/FileStorage/Pictures/"+userid.ToLower()+"/";
-            int startIndex = imgUrl.IndexOf(deletePath, StringComparison.Ordinal);
+            var userid = _decodeToken.Decode(token, "userid");
+            var deletePath = "https://tuongtlc.ddns.net:8080/FileStorage/Pictures/" + userid.ToLower() + "/";
+            var startIndex = imgUrl.IndexOf(deletePath, StringComparison.Ordinal);
 
             if (startIndex >= 0)
             {
-                int length = deletePath.Length;
-                string output =  imgUrl.Remove(startIndex, length) ;
-                string filePath = @"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\" + userid.ToLower() + "\\" + output.ToLower();
+                var length = deletePath.Length;
+                var output = imgUrl.Remove(startIndex, length);
+                var filePath = @"N:\TuongTLCWebsite_Data\Webdata\FileStorage\Pictures\" + userid.ToLower() + "\\" +
+                               output.ToLower();
                 if (File.Exists(filePath))
                 {
                     File.Delete(filePath);
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
 
-            }
-            else
-            {
                 return false;
             }
 
+            return false;
         }
         catch
         {
             return false;
-
         }
     }
-    
 }
