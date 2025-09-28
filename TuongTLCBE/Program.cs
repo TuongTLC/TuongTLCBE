1ef9ff31e979db635b1fae0f5141490c2935abaf
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2025-09-29 01:08:54 +0700

    Change domain name

--- TuongTLCBE/Program.cs
+++ TuongTLCBE/Program.cs
@@ -83,8 +83,8 @@ builder.Services
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-            ValidIssuer = "https://tuongtlc.site",
-            ValidAudience = "https://tuongtlc.site",
+            ValidIssuer = "https://tuongtlc.com",
+            ValidAudience = "https://tuongtlc.com",
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtToken))
         };
     });
